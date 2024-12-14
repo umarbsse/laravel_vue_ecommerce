34fa7ec5e833a531477515d0032b6d870f27e080
umarbsse <umarbsse@gmail.com>
2024-12-14 14:09:00 +0500

    first commit

---
+++ resources/views/components/admin/widgets/header.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<title>Rocker - Bootstrap 5 Admin Dashboard Template</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <x-admin.widgets.css/>
+</head>
